4b44418a24b7dfc8d6bb9f09786f60bd1d5d3eb5
sandy <18382255942@163.com>
2020-05-11 18:45:34 +0800

    fix(): fix log dir name

--- log/file_handler.go
+++ log/file_handler.go
@@ -8,7 +8,7 @@ import (
 )
 
 const (
-	logDir        = "logDir"  // 日志存储目录
+	logDir        = "log_dir" // 日志存储目录
 	logFileName   = "sls.log" // 最新日志存储文件
 	fPrefix       = "sls"
 	RotateLogSize = 64 * 1024 * 1024 // 64M
